eb076afa31f016b4b941ea2bbd31e691a19c943b
Nawfel <79513906+NawfelBC@users.noreply.github.com>
2021-09-09 13:39:58 +0200

    Create KEYS.py

---
+++ KEYS.py
@@ -0,0 +1,4 @@
+TWITTER_API_KEY='insert your Twitter Api Key'
+TWITTER_API_SECRET_KEY='insert your Twitter Api Secret Key'
+TWITTER_ACCESS_TOKEN='insert your Twitter Access Token'
+TWITTER_ACCESS_TOKEN_SCRET='insert your Twitter Access Token Secret'
